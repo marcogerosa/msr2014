e786c16d75576263e2d44505398a6722e124790a
Mauricio Aniche <mauricioaniche@gmail.com>
2014-01-28 19:34:45 -0200

    um monte de mudancas... versao ja bem mais madura

---
+++ stats/run.sh
@@ -0,0 +1,2 @@
+rm *.txt *.png *out
+r CMD BATCH stats-msr2014.r
--- stats/stats-msr2014.r
+++ stats/stats-msr2014.r
@@ -1,26 +1,54 @@
+#library(lattice)
 library(RMySQL)
-library(lattice)
-con <- dbConnect(MySQL(), dbname="aspectj", user='root', password='')
+con <- dbConnect(MySQL(), dbname="msr2014", user='root', password='')
 
-basedir <- "/Users/mauricioaniche/textos/msr2014/"
+basedir <- "/Users/mauricioaniche/textos/msr2014/stats/"
 projects <- c("metricminer", "gnarus", "tubaina")
 
-project <- projects[1]
+calculateAll = function(coverage, project, source) {
 
-sql <- paste("select x.pacote, x.classe, cc, testes, if(testes/cc > 1, 1, testes/cc) as heuristica, emma from (select distinct mc.pacote, mc.classe,(select sum(mc1.cc) from ", project, "_cc mc1 where mc1.classe = mc.classe and mc1.pacote = mc.pacote) as cc,(select sum(m.testes) from ", project, "_aj m where m.classe = mc.classe and m.pacote = mc.pacote) as testes,(select complexity_covered/(complexity_missed + complexity_covered) as emma from ", project, "_emma e where e.class = mc.classe and e.package = mc.pacote) as emma from ", project, "_cc mc) x", sep="")
+	correlation = cor.test(coverage$emma, coverage$heuristica, method = "spearman", paired=true)
+	cat("p value ", correlation$p.value, " estimate ", correlation$estimate, file=paste(basedir, project, "-correlation-", source, ".txt", sep=""))
 
-coverage <- dbGetQuery(con, sql)
+	diferenca <- coverage$heuristica - coverage$emma
+	resumo <- summary(diferenca)
+	cat(resumo, file=paste(basedir, project, "-summary-", source, ".txt", sep=""))
 
-correlation = cor.test(coverage$emma, coverage$heuristica, method = "spearman", paired=true)
+	png(filename=paste(basedir, project, "-histograma-", source, ".png", sep=""))
+	hist(diferenca)
+	axis(side=1, at=seq(-1,1,0.2))
+	dev.off()
 
-diferenca <- coverage$heuristica - coverage$emma
-summary(diferenca)
+	#png(filename=paste(basedir, project, "-histograma-pct-", source, ".png", sep=""))
+	#histogram(diferenca)
+	#dev.off()
 
-png(filename=paste(basedir, project, "-histograma.png", sep=""))
-hist(diferenca)
-axis(side=1, at=seq(-1,1,0.2))
-dev.off()
+}
 
-png(filename=paste(basedir, project, "-histograma-pct.png", sep=""))
-histogram(a)
-dev.off()
+gelato = function(project) {	
+	sql <- paste("select cobertura as heuristica, complexity_covered/(complexity_missed + complexity_covered) as emma from ", project, "_gelato gg join ", project, "_emma ge on gg.classe = ge.class and gg.pacote = ge.package", sep="")
+	
+	coverage <- dbGetQuery(con, sql)
+
+	calculateAll(coverage, project, "gelato")
+	
+}
+
+aspectj = function(project) {
+
+	sql <- paste("select x.pacote, x.classe, cc, testes, if(testes/cc > 1, 1, testes/cc) as heuristica, emma from (select distinct mc.pacote, mc.classe,(select sum(mc1.cc) from ", project, "_cc mc1 where mc1.classe = mc.classe and mc1.pacote = mc.pacote) as cc,(select sum(m.testes) from ", project, "_aj m where m.classe = mc.classe and m.pacote = mc.pacote) as testes,(select complexity_covered/(complexity_missed + complexity_covered) as emma from ", project, "_emma e where e.class = mc.classe and e.package = mc.pacote) as emma from ", project, "_cc mc) x", sep="")
+
+	coverage <- dbGetQuery(con, sql)
+
+	calculateAll(coverage, project, "aspectj")
+
+}
+
+for(i in 1:length(projects)) {
+
+	project <- projects[i]
+
+	aspectj(project)
+	gelato(project)
+	
+}
